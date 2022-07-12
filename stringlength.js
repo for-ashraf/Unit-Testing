fa97d45c7e4a756ca5a3b16f5c693f291428f098
for-ashraf <46405183+for-ashraf@users.noreply.github.com>
2022-07-12 23:14:55 +0500

    Update stringlength.js

--- stringlength.js
+++ stringlength.js
@@ -1,9 +1,7 @@
 function stringLength(myString) {
   if (myString.length > 0 && myString.length <= 10) {
     return true;
-  }
-  else {
-    return false;
-  }
-}
+    }
+  return false;
+    }
 module.exports = stringLength;
